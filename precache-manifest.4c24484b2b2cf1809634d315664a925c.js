c241f7aeeebf298c5b42ec35be9199694bbcd0d3
SIRI H V <lakshmisiri2003@gmail.com>
2022-11-29 17:00:28 +0530

    Updates

---
+++ precache-manifest.4c24484b2b2cf1809634d315664a925c.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "016167d601d112716096",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/css/main.df36a25e.chunk.css"
+  },
+  {
+    "revision": "016167d601d112716096",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/js/main.016167d6.chunk.js"
+  },
+  {
+    "revision": "52077699d4e480884aec",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/js/1.52077699.chunk.js"
+  },
+  {
+    "revision": "5b3dac9b85cc1687de0f",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/css/2.0599411c.chunk.css"
+  },
+  {
+    "revision": "5b3dac9b85cc1687de0f",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/js/2.5b3dac9b.chunk.js"
+  },
+  {
+    "revision": "73b7c8c5d76bf7e7ccb5",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/js/runtime~main.73b7c8c5.js"
+  },
+  {
+    "revision": "5208b9dd3d95900f571ddd1c323f78dd",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/logo.5208b9dd.png"
+  },
+  {
+    "revision": "fe7b0edca9d39c42da4658f404be9511",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/right.fe7b0edc.jpg"
+  },
+  {
+    "revision": "bf00cd7a52fae146b323ff495701033a",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/bgimg.bf00cd7a.jpg"
+  },
+  {
+    "revision": "c41b0a95b127ad44b2db61a1c8256d18",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/burger.c41b0a95.jpg"
+  },
+  {
+    "revision": "915086cf640513adf40b0297ebf725f1",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/it.915086cf.jpg"
+  },
+  {
+    "revision": "1dbf25e5c02c7e763e908bcebbf4ee4e",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/thai.1dbf25e5.jpg"
+  },
+  {
+    "revision": "44f64f0ebf9809af4cbdaff03039ec15",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/ind-meal.44f64f0e.jpg"
+  },
+  {
+    "revision": "43ccca173012801caa2577b248efba51",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/chat.43ccca17.jpeg"
+  },
+  {
+    "revision": "5493d16e2883d273760824136bf23483",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/dessert.5493d16e.webp"
+  },
+  {
+    "revision": "9f8188481bd43dd9788b0e6cb48d48d9",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/toscano.9f818848.jpeg"
+  },
+  {
+    "revision": "aaf1fd7f2690954b0f0bed7449f68f1f",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/venezia.aaf1fd7f.jpeg"
+  },
+  {
+    "revision": "359336056c598cc0a6ee6f215f2c5508",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/chianti.35933605.jpg"
+  },
+  {
+    "revision": "f43c19e1fb77309c418575efea8b8037",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/littleitaly.f43c19e1.jpeg"
+  },
+  {
+    "revision": "e3b7151f7c046a62107e089decabf07d",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/truffles.e3b7151f.png"
+  },
+  {
+    "revision": "cc3fc333f9379b889855adc1372e9af0",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/boss.cc3fc333.jpg"
+  },
+  {
+    "revision": "f7865aba8d5dc609989a6a994044833e",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/leon.f7865aba.jpeg"
+  },
+  {
+    "revision": "1bb35c58b9bb62f0620885223df8077e",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/mcd.1bb35c58.jpg"
+  },
+  {
+    "revision": "3b185ede7b16632a2ed221a71e8dc4fe",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/glens.3b185ede.jpeg"
+  },
+  {
+    "revision": "e20dfdcc709feb59d932e55c9a34259f",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/lakeview.e20dfdcc.jpeg"
+  },
+  {
+    "revision": "90f5f0f8e610abd2e15e79c2dbb8273c",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/deserthouse.90f5f0f8.jpeg"
+  },
+  {
+    "revision": "8459a1302b1a60a59d54815deca58905",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/vv.8459a130.jpeg"
+  },
+  {
+    "revision": "66a58b458ec6760c1ea722306373eb26",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/hari.66a58b45.jpeg"
+  },
+  {
+    "revision": "54416270956fcaa727d4b2e3c828a9f3",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/sairamchat.54416270.jpeg"
+  },
+  {
+    "revision": "a0678c173bb84f8c39e43edc6add17c6",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/mosquerd.a0678c17.jpeg"
+  },
+  {
+    "revision": "2323cb1fed3bc66439441b051b9db66f",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/lanthai.2323cb1f.jpeg"
+  },
+  {
+    "revision": "773821d83f59bb429e3b739e903bd251",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/botai.773821d8.jpeg"
+  },
+  {
+    "revision": "4b48becc92334532ddbb41fe7a900981",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/phra.4b48becc.jpeg"
+  },
+  {
+    "revision": "42cf5e7bdabfd4a8c70774f7bec1756c",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/misu.42cf5e7b.jpeg"
+  },
+  {
+    "revision": "48b1029f45417384b9c647b3b7a5fedd",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/thaivegan.48b1029f.png"
+  },
+  {
+    "revision": "ee322a25a6cc8275bac1e7077ec7a1f1",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/jalpaan.ee322a25.jpg"
+  },
+  {
+    "revision": "f0290ce416b316b7bb0ba3168bf6a20f",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/mtr.f0290ce4.jpeg"
+  },
+  {
+    "revision": "bd629cf3e46c040a53bdb1401641e861",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/paakashala.bd629cf3.jpeg"
+  },
+  {
+    "revision": "675f3e7e98fe6bb3bb2a3e5407d26110",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/karnatic.675f3e7e.jpg"
+  },
+  {
+    "revision": "9f6e2d1ef73dad0e96e1eabf71506583",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/static/media/kapoor.9f6e2d1e.jpeg"
+  },
+  {
+    "revision": "6362ac1151097d2ecf5c75383aaf119c",
+    "url": "/siri-03/Head-Over-Meals---A-Web-Technology-Project/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/siri-03/Head-Over-Meals---A-Web-Technology-Project/precache-manifest.4c24484b2b2cf1809634d315664a925c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/siri-03/Head-Over-Meals---A-Web-Technology-Project/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
